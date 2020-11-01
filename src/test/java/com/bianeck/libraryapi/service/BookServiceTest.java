78ca3f677aa18c3b176cf2584fb2919483af1f37
Thiago Moreira Bianeck <thiagobianeck@gmail.com>
2020-11-01 20:35:24 -0300

    Aula - Criando o teste da camada de serviço

---
+++ src/test/java/com/bianeck/libraryapi/service/BookServiceTest.java
@@ -0,0 +1,35 @@
+package com.bianeck.libraryapi.service;
+
+import com.bianeck.libraryapi.model.entity.Book;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@ExtendWith(SpringExtension.class)
+@ActiveProfiles("test")
+public class BookServiceTest {
+
+    BookService service;
+
+    @Test
+    @DisplayName("Deve salvar um livro")
+    public void saveBookTest() {
+
+        Book book = Book.builder()
+                .isbn("123")
+                .author("Fulano")
+                .title("As aventuras")
+                .build();
+
+        Book savedBook = service.save(book);
+
+        assertThat(savedBook.getId()).isNotNull();
+        assertThat(savedBook.getIsbn()).isEqualTo("123");
+        assertThat(savedBook.getTitle()).isEqualTo("As aventuras");
+        assertThat(savedBook.getAuthor()).isEqualTo("Fulano");
+    }
+}
