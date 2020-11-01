dd159f6171f60fa3a91477a25c644877cec464aa
Thiago Moreira Bianeck <thiagobianeck@gmail.com>
2020-11-01 13:02:30 -0300

    Aula - Controller, DTO e Lombok Project

---
+++ src/main/java/com/bianeck/libraryapi/api/dto/BookDTO.java
@@ -0,0 +1,15 @@
+package com.bianeck.libraryapi.api.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class BookDTO {
+
+    private Long id;
+    private String title;
+    private String author;
+    private String isbn;
+
+}
---
+++ src/main/java/com/bianeck/libraryapi/api/resource/BookController.java
@@ -0,0 +1,24 @@
+package com.bianeck.libraryapi.api.resource;
+
+import com.bianeck.libraryapi.api.dto.BookDTO;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/books")
+public class BookController {
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public BookDTO create() {
+        BookDTO dto = new BookDTO();
+        dto.setAuthor("Autor");
+        dto.setTitle("Meu Livro");
+        dto.setIsbn("1213212");
+        dto.setId(1l);
+        return dto;
+    }
+}
