9d3698be3c2ae383426e95da9a5b060e2824bad8
Thiago Moreira Bianeck <thiagobianeck@gmail.com>
2020-11-04 09:42:30 -0300

    Aula - Deleção de livros

--- src/main/java/com/bianeck/libraryapi/api/resource/BookController.java
+++ src/main/java/com/bianeck/libraryapi/api/resource/BookController.java
@@ -42,6 +42,14 @@ public class BookController {
                 .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND));
     }
 
+    @DeleteMapping("{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void delete(@PathVariable Long id) {
+        Book book = service.getById(id)
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND));
+        service.delete(book);
+    }
+
     @ExceptionHandler(MethodArgumentNotValidException.class)
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     public ApiErrors handleValidationException(MethodArgumentNotValidException ex) {
--- src/test/java/com/bianeck/libraryapi/api/resource/BookControllerTest.java
+++ src/test/java/com/bianeck/libraryapi/api/resource/BookControllerTest.java
@@ -159,6 +159,36 @@ public class BookControllerTest {
 
     }
 
+    @Test
+    @DisplayName("Deve deletar um livro.")
+    public void deleteBookTest() throws Exception{
+
+        BDDMockito.given(service.getById(Mockito.anyLong()))
+                .willReturn(Optional.of(Book.builder().id(1l).build()));
+
+        MockHttpServletRequestBuilder request = MockMvcRequestBuilders
+                .delete(BOOK_API.concat("/" + 1));
+
+        mvc.perform(request)
+                .andExpect(status().isNoContent());
+
+    }
+
+    @Test
+    @DisplayName("Deve retornar ResourceNotFound quando não encontrar o livro para deletar.")
+    public void deleteInexistentBookTest() throws Exception{
+
+        BDDMockito.given(service.getById(Mockito.anyLong()))
+                .willReturn(Optional.empty());
+
+        MockHttpServletRequestBuilder request = MockMvcRequestBuilders
+                .delete(BOOK_API.concat("/" + 1));
+
+        mvc.perform(request)
+                .andExpect(status().isNotFound());
+
+    }
+
 
     private BookDTO createNewBook() {
         return BookDTO.builder()
