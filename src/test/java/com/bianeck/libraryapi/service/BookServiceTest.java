63222648f1d831e5b85afb3115c66f90adac50aa
Thiago Moreira Bianeck <thiagobianeck@gmail.com>
2020-11-04 14:15:11 -0300

    Aula - Teste unitário de obter livro por id na camada de serviço

--- src/test/java/com/bianeck/libraryapi/service/BookServiceTest.java
+++ src/test/java/com/bianeck/libraryapi/service/BookServiceTest.java
@@ -14,6 +14,8 @@ import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
+import java.util.Optional;
+
 import static org.assertj.core.api.Assertions.assertThat;
 
 @ExtendWith(SpringExtension.class)
@@ -69,6 +71,44 @@ public class BookServiceTest {
 
         // verifique que o repository nunca vai executar esse método com esse parâmetro
         Mockito.verify(repository, Mockito.never()).save(book);
+
+    }
+
+    @Test
+    @DisplayName("Deve obter um livro por id.")
+    public void getByIdTest() {
+        
+        Long id = 1l;
+
+        Book book = createValidBook();
+        book.setId(id);
+
+        Mockito.when(repository.findById(id))
+                .thenReturn(Optional.of(book));
+
+        Optional<Book> foundBook = service.getById(id);
+
+        assertThat(foundBook.isPresent()).isTrue();
+        assertThat(foundBook.get().getId()).isEqualTo(id);
+        assertThat(foundBook.get().getAuthor()).isEqualTo(book.getAuthor());
+        assertThat(foundBook.get().getIsbn()).isEqualTo(book.getIsbn());
+        assertThat(foundBook.get().getTitle()).isEqualTo(book.getTitle());
+
+
+    }
+
+    @Test
+    @DisplayName("Deve retornar vazio ao obter um livro por id quando ele não existe na base.")
+    public void bookNotFoundByIdTest() {
+
+        Long id = 1l;
+
+        Mockito.when(repository.findById(id))
+                .thenReturn(Optional.empty());
+
+        Optional<Book> book = service.getById(id);
+
+        assertThat(book.isPresent()).isFalse();
 
     }
 
