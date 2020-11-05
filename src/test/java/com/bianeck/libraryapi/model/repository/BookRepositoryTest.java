d4ab61233bffa7d7ad47be8cb7d086947895a46b
Thiago Moreira Bianeck <thiagobianeck@gmail.com>
2020-11-04 21:29:57 -0300

    Aula - Testes de integração do repository para os métodos save e delete

--- src/test/java/com/bianeck/libraryapi/model/repository/BookRepositoryTest.java
+++ src/test/java/com/bianeck/libraryapi/model/repository/BookRepositoryTest.java
@@ -67,6 +67,35 @@ public class BookRepositoryTest {
 
     }
 
+    @Test
+    @DisplayName("Deve salvar um livro.")
+    public void saveBookTest() {
+
+        Book book = createNewBook("123");
+
+        Book savedBook = repository.save(book);
+
+        assertThat(savedBook.getId()).isNotNull();
+
+    }
+
+    @Test
+    @DisplayName("Deve deletar um livro.")
+    public void deleteBookTest() {
+
+        Book book = createNewBook("123");
+        entityManager.persist(book);
+
+        Book foundBook = entityManager.find(Book.class, book.getId());
+
+        repository.delete(foundBook);
+
+        Book deletedBook = entityManager.find(Book.class, book.getId());
+
+        assertThat(deletedBook).isNull();
+
+    }
+
     private Book createNewBook(String isbn) {
         return Book.builder()
                 .title("As aventuras")
