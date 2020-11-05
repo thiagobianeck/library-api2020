b6a117b55e5ef55e6514dd5145f8bddd6607b720
Thiago Moreira Bianeck <thiagobianeck@gmail.com>
2020-11-04 23:05:48 -0300

    Aula - Implementando a consulta de livros

--- src/main/java/com/bianeck/libraryapi/api/resource/BookController.java
+++ src/main/java/com/bianeck/libraryapi/api/resource/BookController.java
@@ -6,6 +6,9 @@ import com.bianeck.libraryapi.exception.BusinessException;
 import com.bianeck.libraryapi.model.entity.Book;
 import com.bianeck.libraryapi.service.BookService;
 import org.modelmapper.ModelMapper;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.MethodArgumentNotValidException;
@@ -13,7 +16,9 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
 
 import javax.validation.Valid;
+import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/api/books")
@@ -61,6 +66,18 @@ public class BookController {
                 })
                 .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND));
 
+    }
+
+    @GetMapping
+    public Page<BookDTO> find(BookDTO dto, Pageable pageRequest) {
+        Book filter = modelMapper.map(dto, Book.class);
+        Page<Book> result = service.find(filter, pageRequest);
+        List<BookDTO> list = result.getContent()
+                .stream()
+                .map(entity -> modelMapper.map(entity, BookDTO.class))
+                .collect(Collectors.toList());
+
+        return new PageImpl<BookDTO>(list, pageRequest, result.getTotalElements());
     }
 
     @ExceptionHandler(MethodArgumentNotValidException.class)
