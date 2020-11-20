117afd2af45f9ac371e43d94f48cf5550feeae21
Thiago Moreira Bianeck <thiagobianeck@gmail.com>
2020-11-20 17:47:57 -0300

    Aula - Publicando as modificações no GITHUB

--- src/main/java/com/bianeck/libraryapi/LibraryApiApplication.java
+++ src/main/java/com/bianeck/libraryapi/LibraryApiApplication.java
@@ -1,35 +1,18 @@
 package com.bianeck.libraryapi;
 
-import com.bianeck.libraryapi.service.EmailService;
 import org.modelmapper.ModelMapper;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
-import java.util.Arrays;
-import java.util.List;
-
 @SpringBootApplication
 @EnableScheduling
 public class LibraryApiApplication {
 
-	@Autowired
-	private EmailService emailService;
-
 	@Bean
 	public ModelMapper modelMapper() {
 		return new ModelMapper();
-	}
-
-	@Bean
-	public CommandLineRunner runner() {
-		return args -> {
-			List<String> emails = Arrays.asList("468ac2940f-d99202@inbox.mailtrap.io");
-			emailService.sendMails("Testando serviço de emails.", emails);
-		};
 	}
 
 	public static void main(String[] args) {
