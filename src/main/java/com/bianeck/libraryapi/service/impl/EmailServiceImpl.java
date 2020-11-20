ae4d39d4242bbf1b8a4fb94067e3ef0182b5d465
Thiago Moreira Bianeck <thiagobianeck@gmail.com>
2020-11-20 17:33:50 -0300

    Aula - Implementando o serviço de email

--- src/main/java/com/bianeck/libraryapi/service/impl/EmailServiceImpl.java
+++ src/main/java/com/bianeck/libraryapi/service/impl/EmailServiceImpl.java
@@ -1,15 +1,37 @@
 package com.bianeck.libraryapi.service.impl;
 
 import com.bianeck.libraryapi.service.EmailService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
+@RequiredArgsConstructor
 public class EmailServiceImpl implements EmailService {
+
+    @Value("${application.mail.default-remetent}")
+    private String remetent;
+
+    @Value("${application.mail.lateloans.message}")
+    private String message;
+
+    private final JavaMailSender javaMailSender;
 
     @Override
     public void sendMails(String mensagem, List<String> mailsList) {
 
+        String[] mails = mailsList.toArray(new String[mailsList.size()]);
+
+        SimpleMailMessage mailMessage = new SimpleMailMessage();
+        mailMessage.setFrom(remetent);
+        mailMessage.setSubject("Livro com empréstimo atrasado.");
+        mailMessage.setText(message);
+        mailMessage.setTo(mails);
+
+        javaMailSender.send(mailMessage);
     }
 }
